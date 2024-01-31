c570a91de3edd98e2770e7f84de96d3e07c1acc7
Sasanka <31107620+psasanka1729@users.noreply.github.com>
2024-01-30 21:57:53 -0600

    large potential landscape

--- large_potential_landscape.py
+++ large_potential_landscape.py
@@ -5,6 +5,20 @@ import matplotlib.pyplot as plt
 from scipy import fftpack
 import scipy.sparse
 
+# %%
+# matplotlib parameters 
+large = 40; med = 60; small = 20
+params = {'axes.titlesize': med,
+          'axes.titlepad' : med,
+          'legend.fontsize': med,
+          'axes.labelsize': med ,
+          'axes.titlesize': med ,
+          'xtick.labelsize': med ,
+          'ytick.labelsize': med ,
+          'figure.titlesize': med}
+plt.rcParams['text.usetex'] = True # need LaTeX. Change it to False if LaTeX is not installed in the system
+plt.rcParams.update(params)
+
 # %% [markdown]
 # ## Source well parameters
 
@@ -20,7 +34,7 @@ omega_x = trap_frequency
 trap_length = np.sqrt(H_BAR/(M*trap_frequency)) # m
 A = PI*trap_length**2 # m*m
 
-N_atom = 40000
+N_atom = 10**5
 
 # interaction strength in the source well.
 g_source   = (4*PI*H_BAR**2*a_s)/(A*M)
@@ -38,10 +52,10 @@ print(delta)
 
 # %%
 # number of discretized intervals in the position array
-N = 2**12 # choose a power of two to make things simpler for fast Fourier transform
+N = 2**15 # choose a power of two to make things simpler for fast Fourier transform
 
 # barrier height outside the trap.
-infinite_barrier_height = 10**7*10**3*2*PI*H_BAR
+infinite_barrier_height = 10**5*10**3*2*PI*H_BAR
 
 
 position_start = -41*1.e-6/x_s # m
@@ -79,11 +93,18 @@ for i in range(N_barrier_to_source_start,N_source_start_to_source_end):
 N_source_well_end_to_position_end = N_source_start_to_source_end + int(abs(position_end-source_well_end)/dx)
 for i in range(N_source_start_to_source_end,N_source_well_end_to_position_end):
     potential_arr[i] = infinite_barrier_height    
-  
+
+f = plt.figure()    
 
 # changing potential into dimensionless unit
 potential_arr = potential_arr/(epsilon*M*omega_x**2*x_s**2)
 
+plt.plot(position_arr,potential_arr,linewidth=3)
+plt.xlabel(r"Position, $x$")
+plt.ylabel(r"Potential, $V_{\textrm{Source}}$")
+f.set_figwidth(12)
+f.set_figheight(8)
+plt.show()
 
 # %% [markdown]
 # ## time split code
@@ -152,21 +173,64 @@ psi_initial = np.ones(N)
 psi_initial = normalize_x(psi_initial) 
 
 # wavefunction is evolved in imaginary time to get the ground state
-final_time_SI = 0.1
-time_step_SI  = -1j*10**(-8)   
+final_time_SI = 0.01
+time_step_SI  = -1j*10**(-7)   
 final_time = omega_x*final_time_SI
 time_step = omega_x*time_step_SI
 psi_ITE = time_split_suzukui_trotter(psi_initial,potential_arr,time_step,final_time);
-#print("Normalization of the wavefucntion = ",np.sqrt(np.sum(np.abs(psi_ITE)**2)*dx) )
-
-# %
+print("Normalization of the wavefucntion = ",np.sqrt(np.sum(np.abs(psi_ITE)**2)*dx) )
 
 # %%
+f = plt.figure()
+f.set_figwidth(17)
+f.set_figheight(11)
+plt.plot(position_arr,np.abs(psi_ITE)**2,label="$\psi_{0}(x)$",linewidth=5,color="red")
+plt.legend()
+plt.title("Ground state in the source well, "+"$N_{atom} = $"+str(N_atom))
+plt.ylabel(r"Wavefunction, $\tilde{\psi(\tilde{x})}$",labelpad=20)
+plt.xlabel(r"Position, $\tilde{x}$",labelpad=20)
+ax = f.gca()
+ax.axvline(source_well_start, color="green",label = r"Infinite well boundary",linestyle="--",linewidth = 4)
+ax.axvline(source_well_end, color="green",label = "",linestyle="--",linewidth = 4)
+plt.legend()
+for spine in ax.spines.values():
+    spine.set_linewidth(3)
+#plt.xlim([-0.05,source_well_end+0.05])
+plt.gcf().subplots_adjust(bottom=0.2)
+plt.tick_params(axis="x", direction="inout", length=20, width=5, color="k")
+plt.tick_params(axis="y", direction="inout", length=20, width=5, color="k")
+#plt.xlim([0.0-0.05,source_well_end+0.05])
+#plt.savefig("ground_state_in_source_well_imaginary_time_evolution.jpg", dpi=300)
+#print("Normalization of the wavefunction = ",np.sum(np.abs(psi_ITE)**2)*dx)
+plt.show()
+
+# %%
+"""f = plt.figure()
+f.set_figwidth(20)
+f.set_figheight(15)
+plt.plot(position_arr,np.abs(psi_ITE/np.sqrt(x_s))**2,label="$\psi_{0}(x)$",linewidth=5,color="red")
+plt.legend()
+plt.title("Ground state in the source well, "+"$N_{atom} = $"+str(N_atom))
+plt.ylabel(r"$\psi(x)$")
+plt.xlabel(r"$x\; (\mu m)$" )
+ax = f.gca()
+ax.axvline(0.0/x_s, color="green",label = r"Infinite well boundary",linestyle="--")
+ax.axvline(source_well_end, color="green",label = r"Infinite well boundary",linestyle="--")
+for spine in ax.spines.values():
+    spine.set_linewidth(4)
+plt.gcf().subplots_adjust(bottom=0.2)
+plt.xlim([0.0-0.05,source_well_end+0.05])
+plt.tick_params(axis="x", direction="inout", length=20, width=3, color="k")
+plt.tick_params(axis="y", direction="inout", length=20, width=3, color="k")
+#plt.savefig("ground_state_in_source_well_imaginary_time_evolution.jpg", dpi=300)
+#print("Normalization of the wavefunction = ",np.sum(np.abs(psi_ITE)**2)*dx)
+plt.show()""";
 
 # %% [markdown]
 # # transistor potential landscape
 
 # %%
+x_s
 
 # %%
 # height of the infinite barrier in the source and the drain well.
@@ -243,6 +307,20 @@ for i in range(N_drain_start_to_drain_well_end,N_drain_end_to_barrier):
 # making the potential dimensionless       
 #source_gate_drain_well_potential = source_gate_drain_well_potential/(epsilon*M*omega_x**2*x_s**2)   
 
+# %%
+f = plt.figure()
+plt.plot(source_gate_drain_well_position,source_gate_drain_well_potential/(epsilon*M*omega_x**2*x_s**2),linewidth=3,color="b")
+plt.xlabel(r"Position, $(\mu  m)$")
+plt.ylabel(r"Potential (kHz)")
+plt.ylim([0,GD_barrier_height/(epsilon*M*omega_x**2*x_s**2)*1.1])
+f.set_figwidth(15)
+f.set_figheight(7)
+ax = f.gca()
+for spine in ax.spines.values():
+    spine.set_linewidth(1.9)
+plt.gcf().subplots_adjust(bottom=0.2)
+#plt.savefig('transistor_potential_step_gate_well_infinite_barrier.jpg', dpi=600)
+plt.show() 
 
 # %% [markdown]
 # # time split to evolve the wavefunction in real time
@@ -259,9 +337,8 @@ while len(psi_initial_for_full_potential) < N:
     psi_initial_for_full_potential = np.hstack((psi_initial_for_full_potential,np.array([0])))
     
 
-
 final_time_SI = 10*10**(-3)
-time_step_SI  = 10**(-8)  
+time_step_SI  = 10**(-7)  
 # time is made dimensionless  
 final_time = omega_x*final_time_SI
 time_step = omega_x*time_step_SI
@@ -269,7 +346,75 @@ time_evolved_wavefunction_time_split = time_split_suzukui_trotter(psi_initial_fo
                                         source_gate_drain_well_potential,
                                         time_step,final_time)
 
-np.save("psi_t_time_split.npy",time_evolved_wavefunction_time_split)
+# %%
+print("Normalization of the time evolved wavefunction = ",np.sum(np.abs(time_evolved_wavefunction_time_split)**2)*dx)
+
+# %%
+data0 = source_gate_drain_well_position
+data1 = N_atom*np.abs(time_evolved_wavefunction_time_split)**2*dx
+#data2 = np.abs(time_evolved_wavefunction_time_split)**2
+data3 = source_gate_drain_well_potential/(epsilon*M*omega_x**2*x_s**2)
+
+fig, ax1 = plt.subplots()
+
+ax1.set_xlabel(r"Position, $\tilde{x}$")
+ax1.set_ylabel(r"$|\tilde{\psi}|^{2}$", color="tab:red")
+#ax1.scatter(t, data1, color=color,s= 5,linewidth = 3,label=r"Time = ")
+#ax1.plot(data0, data2, color="tab:red",linewidth = 3,label=r"Time split")
+ax1.plot(data0, data1, color="tab:red",linewidth = 5,label=r"Time split wavefunction")
+plt.legend()
+#ax1.set_xlim([-0.1, drain_well_end+0.01]) 
+ax1.tick_params(axis='y', labelcolor="tab:red")
+ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+
+color = "tab:blue"
+ax2.set_ylabel(r"$V(x)/(\epsilon \omega^{2}_{x} x^{2}_{s})$ ", color=color)  # we already handled the x-label with ax1
+ax2.plot(data0, data3, color=color,linewidth = 5)
+#ax1.set_xlim([-0.05, 2.6])
+ax1.set_ylim([0, np.max(data1)*1.1])
+ax2.set_ylim([0, GD_barrier_height/(epsilon*M*omega_x**2*x_s**2)*1.1])
+ax2.tick_params(axis="y", labelcolor=color)
+fig.set_figwidth(30)
+fig.set_figheight(10)
+plt.subplots_adjust(bottom=0.2)
+for spine in ax1.spines.values():
+    spine.set_linewidth(3)
+ax1.tick_params(axis="x", direction="inout", length=20, width=4, color="k")
+ax1.tick_params(axis="y", direction="inout", length=20, width=4, color="k")
+#plt.savefig("psi_"+str(N_atom)+"_"+str(i)+".jpg", dpi=600)
+fig.tight_layout()
+
+# %%
+np.sum(N_atom*np.abs(time_evolved_wavefunction_time_split)**2*dx)
+
+# %%
+# function to calculate the number of atoms in each well
+def number_of_atom(wavefunction,a,b):
+    psi_lst = []
+    for i in range(N):
+        if a <= source_gate_drain_well_position[i] <= b:
+            psi_lst.append(wavefunction[i])
+    return N_atom*np.sum(np.abs(psi_lst)**2)*dx
+
+# %%
+r"""
+
+Number of atoms in each well calculated using time split wavefunction.
+
+"""
+
+print("Number of atoms in source well =",
+      number_of_atom(time_evolved_wavefunction_time_split,source_well_start ,source_gate_barrier_start))
+print("Number of atoms in gate well =",
+      number_of_atom(time_evolved_wavefunction_time_split,gate_well_start ,gate_well_end))
+print("Number of atoms in drain well =",
+      number_of_atom(time_evolved_wavefunction_time_split,drain_well_start ,drain_well_end))
+
+# %% [markdown]
+# # Runge Kutta algorithm for time evolution
+
+# %% [markdown]
+# ## Custom RK4
 
 # %%
 D2 = scipy.sparse.diags([1, -2, 1], 
@@ -288,24 +433,8 @@ def dpsi_dt(t,psi):
                 (source_gate_drain_well_potential/(epsilon*M*omega_x**2*x_s**2))*psi + 
                 delta*epsilon**(3/2)*np.abs(psi)**2*psi)
 
-
-psi_0 = np.complex64(psi_initial_for_full_potential)
-psi_0 = normalize_x(psi_0)
-#dt = 1.e-4
-psi_t = psi_0
 t0 = 0.0
-#t_eval = np.arange(t0, final_time, dt)
-t_eval = np.linspace(t0,final_time,10)
-sol = scipy.integrate.solve_ivp(dpsi_dt, 
-                                t_span = [t0, final_time],
-                                y0 = psi_0, 
-                                t_eval = t_eval,
-                                method="RK45")
-
-np.save("psi_t_scipy_rk4.npy",sol[:,-1])
-
-t0 = 0.0
-time_step_SI  = 10**(-9)    
+time_step_SI  = 10**(-8)    
 time_step = omega_x*time_step_SI
 def wavefunction_t(total_time):
     # initial wavefunction
@@ -333,14 +462,89 @@ def wavefunction_t(total_time):
 # %%
 time_evolved_wavefunction_rk4 = wavefunction_t(final_time)
 
-np.save("psi_t_rk4.npy",time_evolved_wavefunction_rk4)
+# %%
+data0 = source_gate_drain_well_position
+data1 = N_atom*np.abs(time_evolved_wavefunction_rk4)**2*dx
+#data2 = np.abs(time_evolved_wavefunction_time_split)**2
+data3 = source_gate_drain_well_potential/(epsilon*M*omega_x**2*x_s**2)
 
+fig, ax1 = plt.subplots()
+
+ax1.set_xlabel(r"Position, $\tilde{x}$")
+ax1.set_ylabel(r"$|\tilde{\psi}|^{2}$", color="tab:red")
+#ax1.scatter(t, data1, color=color,s= 5,linewidth = 3,label=r"Time = ")
+#ax1.plot(data0, data2, color="tab:red",linewidth = 3,label=r"Time split")
+ax1.plot(data0, data1, color="tab:red",linewidth = 5,label=r"Time evolved wavefunction")
+plt.legend()
+#ax1.set_xlim([-0.1, drain_well_end+0.01]) 
+ax1.set_ylim([0, np.max(data1)*1.1])
+ax1.tick_params(axis='y', labelcolor="tab:red")
+ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+
+color = "tab:blue"
+ax2.set_ylabel(r"$V(x)/(\epsilon \omega^{2}_{x} x^{2}_{s})$ ", color=color)  # we already handled the x-label with ax1
+ax2.plot(data0, data3, color=color,linewidth = 5)
+ax2.set_ylim([0, GD_barrier_height/(epsilon*M*omega_x**2*x_s**2)*1.1])
+ax2.tick_params(axis="y", labelcolor=color)
+fig.set_figwidth(30)
+fig.set_figheight(10)
+plt.subplots_adjust(bottom=0.2)
+for spine in ax1.spines.values():
+    spine.set_linewidth(3)
+ax1.tick_params(axis="x", direction="inout", length=20, width=4, color="k")
+ax1.tick_params(axis="y", direction="inout", length=20, width=4, color="k")
+#plt.savefig("psi_"+str(N_atom)+"_"+str(i)+".jpg", dpi=600)
+fig.tight_layout()
 
 # %% [markdown]
 # ## RK4 of scipy
 
 # %%
+psi_0 = np.complex64(psi_initial_for_full_potential)
+psi_0 = normalize_x(psi_0)
+#dt = 1.e-4
+psi_t = psi_0
+t0 = 0.0
+#t_eval = np.arange(t0, final_time, dt)
+t_eval = np.linspace(t0,final_time,10)
+sol = scipy.integrate.solve_ivp(dpsi_dt, 
+                                t_span = [t0, final_time],
+                                y0 = psi_0, 
+                                t_eval = t_eval,
+                                method="RK45")
 
+# %%
+data0 = source_gate_drain_well_position
+data1 = N_atom*np.abs(sol.y[:,-1])**2*dx
+data3 = source_gate_drain_well_potential/(epsilon*M*omega_x**2*x_s**2)
+
+fig, ax1 = plt.subplots()
+
+ax1.set_xlabel(r"Position, $\tilde{x}$")
+ax1.set_ylabel(r"$|\tilde{\psi}|^{2}$", color="tab:red")
+#ax1.scatter(t, data1, color=color,s= 5,linewidth = 3,label=r"Time = ")
+#ax1.plot(data0, data2, color="tab:red",linewidth = 3,label=r"Time split")
+ax1.plot(data0, data1, color="tab:red",linewidth = 5,label=r"Time evolved wavefunction")
+plt.legend()
+#ax1.set_xlim([-0.1, drain_well_end+0.01]) 
+ax1.set_ylim([0, np.max(data1)*1.1])
+ax1.tick_params(axis='y', labelcolor="tab:red")
+ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+
+color = "tab:blue"
+ax2.set_ylabel(r"$V(x)/(\epsilon \omega^{2}_{x} x^{2}_{s})$ ", color=color)  # we already handled the x-label with ax1
+ax2.plot(data0, data3, color=color,linewidth = 5)
+ax2.set_ylim([0, GD_barrier_height/(epsilon*M*omega_x**2*x_s**2)*1.1])
+ax2.tick_params(axis="y", labelcolor=color)
+fig.set_figwidth(30)
+fig.set_figheight(10)
+plt.subplots_adjust(bottom=0.2)
+for spine in ax1.spines.values():
+    spine.set_linewidth(3)
+ax1.tick_params(axis="x", direction="inout", length=20, width=4, color="k")
+ax1.tick_params(axis="y", direction="inout", length=20, width=4, color="k")
+#plt.savefig("psi_"+str(N_atom)+"_"+str(i)+".jpg", dpi=600)
+fig.tight_layout()
 
 # %% [markdown]
 # ## 
