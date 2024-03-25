e81f9d1d33eb07d2b2778415575cc1175b0eb487
Sasanka Dowarah <sxd190113@login-01.localdomain>
2024-03-24 23:39:36 -0500

    Commit before run real_time_evolution

--- build/real_time_evolution/gaussian_source_well_resonance_test.py
+++ build/real_time_evolution/gaussian_source_well_resonance_test.py
@@ -407,6 +407,7 @@ L = np.abs((complete_transistor_position[-1]-complete_transistor_position[0]))
 dx = L/N
 np.save("dx2.npy",dx)
 # momentum space discretization
+dk = (2*PI)/L
 k = np.hstack([np.arange(0,N/2), np.arange(-N/2,0)])*dk
     
 E_k = k**2*epsilon/2
@@ -417,14 +418,14 @@ while len(psi_initial_for_full_potential) < N:
     psi_initial_for_full_potential = np.hstack((psi_initial_for_full_potential,np.array([0])))
     
 
-final_time_SI = 20*10**(-3)
-time_step_SI  = 10**(-7)  
+final_time_SI = 30*10**(-3)
+time_step_SI  = 10**(-8)  
 # time is made dimensionless  
 final_time = OMEGA_X*final_time_SI
 time_step = OMEGA_X*time_step_SI
 time_evolved_wavefunction_time_split = time_split_suzukui_trotter(psi_initial_for_full_potential,
                                         complete_transistor_potential*10**3*2*PI*H_BAR,
-                                        time_step, final_time, [t for t in range(20)])
+                                        time_step, final_time, [t for t in range(30)])
 
 # %%
 # plotting everything in SI units
