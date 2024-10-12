f04d11d9662daa9ce40336e5123a09112e8ef273
Sasanka Dowarah <sxd190113@login-01.localdomain>
2024-10-12 12:02:02 -0500

    Commit before run imaginary_time_evolution

--- build/imaginary_time_evolution/imaginary_time_evolution_builder.py
+++ build/imaginary_time_evolution/imaginary_time_evolution_builder.py
@@ -5,7 +5,8 @@ import numpy
 import os
 
 #partition_info=['cmt',16] # = [partition,ncores]
-partition_info=['gpu-preempt',16] # = [partition,ncores]
+#partition_info=['gpu-preempt',16] # = [partition,ncores]
+partition_info=['cpu-preempt',16]
 time_str='7-00:00:00'
 project_name=os.getcwd().split('/')[-3]
 myemail=os.environ["MYEMAIL"]
