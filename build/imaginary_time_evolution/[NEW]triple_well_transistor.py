08f959a7823678eab4cf1f518a6d6defdba057b9
Sasanka Dowarah <sxd190113@login-01.localdomain>
2024-10-28 18:26:58 -0500

    Commit before run imaginary_time_evolution

--- build/imaginary_time_evolution/[NEW]triple_well_transistor.py
+++ build/imaginary_time_evolution/[NEW]triple_well_transistor.py
@@ -35,7 +35,7 @@ class GrossPitaevskiiSolver:
         self.omega_l = 2 * np.pi * 1178  # rad/s # Longitudinal trapping frequency.
         self.number_of_atoms = number_of_atoms # Number of atoms in the trap.
         self.atom_mass = 1.4192261 * 10 ** (-25)  # kg # Mass of Rubidium-87 atom.
-        self.a_s = 98.006*5.29177210544*1.e-11 * 0.001 # m # Scattering length of Rubidium-87 atom.
+        self.a_s = 98.006*5.29177210544*1.e-11 * 0.05 # m # Scattering length of Rubidium-87 atom.
 
         # Parameters for the dimensionless form of the Gross-Pitaevskii equation.
         self.l_0 = np.sqrt(self.h_bar / (self.atom_mass * self.omega_l))
@@ -453,7 +453,7 @@ barrier_height_GD =  33 #V_GD_lst[V_GD_index]# In kHz units.
 np.save("barrier_height_SG.npy", barrier_height_SG)
 np.save("barrier_height_GD.npy", barrier_height_GD)
 
-source_bias_lst = np.linspace(25,30,64)
+source_bias_lst = np.linspace(19,24,64)
 np.save("source_bias_lst.npy", source_bias_lst)
 source_bias_index = int(sys.argv[1])
 
