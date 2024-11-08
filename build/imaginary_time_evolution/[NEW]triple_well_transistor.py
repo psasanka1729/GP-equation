e0f2445024f49779063b6cf147e4c631e8e4e4bd
Sasanka Dowarah <sxd190113@ganymede.utdallas.edu>
2024-11-08 11:53:04 -0600

    Commit before run imaginary_time_evolution

--- build/imaginary_time_evolution/[NEW]triple_well_transistor.py
+++ build/imaginary_time_evolution/[NEW]triple_well_transistor.py
@@ -35,7 +35,7 @@ class GrossPitaevskiiSolver:
         self.omega_l = 2 * np.pi * 1178  # rad/s # Longitudinal trapping frequency.
         self.number_of_atoms = number_of_atoms # Number of atoms in the trap.
         self.atom_mass = 1.4192261 * 10 ** (-25)  # kg # Mass of Rubidium-87 atom.
-       	self.a_s = 98.006*5.29177210544*1.e-11 * 0.01 # m # Scattering length of Rubidium-87 atom.
+       	self.a_s = 98.006*5.29177210544*1.e-11 * 0.01 * 1/2 # m # Scattering length of Rubidium-87 atom.
         # Parameters for the dimensionless form of the Gross-Pitaevskii equation.
         self.l_0 = np.sqrt(self.h_bar / (self.atom_mass * self.omega_l))
         self.t_0 = 1 / self.omega_l
@@ -446,12 +446,12 @@ position_arr = np.linspace(position_start,position_end,N)*1.e-6
 np.save("transistor_position_arr.npy", position_arr)
 
 barrier_height_SG = 31 # In kHz units.
-barrier_height_GD = 31 # In kHz units.
+barrier_height_GD = 33 # In kHz units.
 
 np.save("barrier_height_SG.npy", barrier_height_SG)
 np.save("barrier_height_GD.npy", barrier_height_GD)
 
-source_bias_lst = np.linspace(24,29,64)
+source_bias_lst = np.linspace(23,30,64)
 np.save("source_bias_lst.npy", source_bias_lst)
 source_bias_index = int(sys.argv[1])
 
--- build/imaginary_time_evolution/imaginary_time_evolution_builder.py
+++ build/imaginary_time_evolution/imaginary_time_evolution_builder.py
@@ -4,9 +4,9 @@ import subprocess
 import numpy
 import os
 
-#partition_info=['cmt',16] # = [partition,ncores]
+partition_info=['normal',16] # = [partition,ncores]
 #partition_info=['gpu-preempt',16] # = [partition,ncores]
-partition_info=['256h',16]
+#partition_info=['256h',16]
 time_str='4-00:00:00'
 project_name=os.getcwd().split('/')[-3]
 myemail=os.environ["MYEMAIL"]
