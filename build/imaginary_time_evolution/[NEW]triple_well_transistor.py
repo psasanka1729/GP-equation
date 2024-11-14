4590591ec3b4dc665d01d30ab08ea3c9a70c0178
Sasanka Dowarah <sxd190113@ganymede.utdallas.edu>
2024-11-14 12:39:06 -0600

    Commit before run imaginary_time_evolution

--- build/imaginary_time_evolution/[NEW]triple_well_transistor.py
+++ build/imaginary_time_evolution/[NEW]triple_well_transistor.py
@@ -25,7 +25,8 @@ plt.rcParams.update(params)
 PI = np.pi
 H_BAR = 1.0545718 * 10 ** (-34)
 
-a_s_lst = np.linspace(0, 100, 64)
+a_s_lst = np.linspace(0, 4, 64)
+np.save("a_s_lst.npy",a_s_lst)
 a_s_index = int(sys.argv[1])
 a_s_factor = a_s_lst[a_s_index]
 np.save("a_s_factor.npy",a_s_factor)
@@ -40,7 +41,7 @@ class GrossPitaevskiiSolver:
         self.omega_l = 2 * np.pi * 1178  # rad/s # Longitudinal trapping frequency.
         self.number_of_atoms = number_of_atoms # Number of atoms in the trap.
         self.atom_mass = 1.4192261 * 10 ** (-25)  # kg # Mass of Rubidium-87 atom.
-       	self.a_s = 98.006*5.29177210544*1.e-11 * a_s_factor # m # Scattering length of Rubidium-87 atom.
+       	self.a_s = 98.006*5.29177210544*1.e-11 * 0.01 * a_s_factor # m # Scattering length of Rubidium-87 atom.
         # Parameters for the dimensionless form of the Gross-Pitaevskii equation.
         self.l_0 = np.sqrt(self.h_bar / (self.atom_mass * self.omega_l))
         self.t_0 = 1 / self.omega_l
@@ -451,7 +452,7 @@ position_arr = np.linspace(position_start,position_end,N)*1.e-6
 np.save("transistor_position_arr.npy", position_arr)
 
 barrier_height_SG = 31 # In kHz units.
-barrier_height_GD = 33 # In kHz units.
+barrier_height_GD = 31 # In kHz units.
 
 np.save("barrier_height_SG.npy", barrier_height_SG)
 np.save("barrier_height_GD.npy", barrier_height_GD)
@@ -460,7 +461,7 @@ np.save("barrier_height_GD.npy", barrier_height_GD)
 #np.save("source_bias_lst.npy", source_bias_lst)
 #source_bias_index = int(sys.argv[1])
 
-source_bias = 15 #source_bias_lst[source_bias_index]  # In kHz units.
+source_bias = 26 #source_bias_lst[source_bias_index]  # In kHz units.
 np.save("source_bias.npy", source_bias)
 
 complete_transistor_potential = transistor_potential_landscape(source_bias, position_arr*1.e6, barrier_height_SG, barrier_height_GD, 0.0)*10**3*H_BAR*2*PI # In SI units.
